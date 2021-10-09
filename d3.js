a6b9f0489bb709e219636489b1d11d3d5fceb7b7
Joel Kramer <joel@nuffsaid.com>
2021-10-08 21:19:59 -0700

    ...

--- d3.js
+++ d3.js
@@ -2,7 +2,27 @@
 
 const commandLineArgs = require('command-line-args');
 const commandLineUsage = require('command-line-usage');
-const { setOptions, addUser } = require('./lib/da3000');
+const { setOptions, mockHTTP } = require('./lib/da3000');
+const {
+  EVENT_TYPE_ANY,
+  EVENT_TYPE_ADD,
+  EVENT_TYPE_DELETE,
+  EVENT_TYPE_MUTATE,
+
+  ENTITY_TYPE_USER,
+  ENTITY_TYPE_COMMENT,
+  ENTITY_TYPE_MUTATION,
+} = require('./constants');
+const {
+  debug: {
+    any: debugAny,
+    cli: debug,
+  }
+} = require('./config');
+const eventClasses = require('./lib/model/events');
+const entityClasses = require('./lib/model/entities');
+const Server = require('./Server');
+const Client = require('./Client');
 
 const optionDefinitions = [
   {
@@ -24,6 +44,12 @@ const optionDefinitions = [
     alias: 'u',
     type: String,
     description: 'Add user'
+  },
+  {
+    name: 'entity',
+    alias: 'e',
+    type: String,
+    description: 'Lookup entity'
   },
   {
     name: 'test',
@@ -54,6 +80,62 @@ if (options.help || noOptions) {
     }
   ])
   console.log(usage)
-} else if (options.adduser) {
-  addUser(options.adduser);
+} else {
+  const server = new Server();
+  const client = new Client({
+    username: 'Bob',
+    password: 'xyzzy',
+  });
+  function addEntity(entity) {
+    return client.call('addEntity', entity);
+  }
+  server
+    .initialize()
+    .then(() => client.initialize())
+    .then(() => {
+      if (options.adduser) {
+        addUser(options.adduser);
+      } else if (options.test) {
+        client
+          .call('getEntityById', { id: 1 })
+          .then(console.log)
+          .catch(log)
+        ;
+      } else if (options.entity) {
+        client
+          .call('getEntityById', { id: parseInt(options.entity) })
+          .then(console.log)
+          .catch(log)
+        ;
+      }
+    });
+}
+
+function addUser(username) {
+  return mockHTTP(makeEvent(EVENT_TYPE_ADD, ENTITY_TYPE_USER, username));
+}
+
+function mutateUser(args) {
+  return mockHTTP(makeEvent(EVENT_TYPE_MUTATE, ENTITY_TYPE_MUTATION, args));
+}
+
+function addComment(args) {
+  return mockHTTP(makeEvent(EVENT_TYPE_ADD, ENTITY_TYPE_COMMENT, args));
+}
+
+function makeEvent(eventType, entityType, payload) {
+  const event = new eventClasses[eventType](
+    new entityClasses[entityType](payload)
+  );
+
+  return JSON.stringify(event);
+}
+
+function log() {
+  if (debug && debugAny) {
+    console.log.apply(console, [
+      'D³CLI:',
+      ...arguments,
+    ]);
+  }
 }
