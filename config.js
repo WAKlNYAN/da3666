1ff3b0d46feb18c8919f89e789e7d738cdd36abb
Joel Kramer <joel@nuffsaid.com>
2021-10-11 14:58:00 -0700

    ...

--- config.js
+++ config.js
@@ -1,3 +1,44 @@
+const cliOptions = [
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    description: 'Display this usage guide.'
+  },
+  {
+    name: 'file',
+    alias: 'f',
+    type: String,
+    multiple: false,
+    description: 'Database file to use',
+    typeLabel: '<files>'
+  },
+  {
+    name: 'adduser',
+    alias: 'u',
+    type: String,
+    description: 'Add user'
+  },
+  {
+    name: 'entity',
+    alias: 'e',
+    type: String,
+    description: 'Lookup entity'
+  },
+  {
+    name: 'test',
+    alias: 't',
+    type: Boolean,
+    description: 'Run Test'
+  },
+  {
+    name: 'rpc',
+    alias: 'r',
+    type: Boolean,
+    description: 'List RPC methods'
+  }
+];
+
 module.exports = {
   ws: {
     port: 8081,
@@ -9,6 +50,22 @@ module.exports = {
     client: true,
     server: true,
     serverStore: true,
+  },
+  cli: {
+    usage: [
+      {
+        header: 'DA3666',
+        content: 'D3 Demonstration CLI'
+      },
+      {
+        header: 'Options',
+        optionList: cliOptions,
+      },
+      {
+        content: 'Project home: {underline https://github.com/WAKlNYAN/da3666}\n'
+      }
+    ],
+    options: cliOptions,
   },
   db: './db.json',
 };
--- d3.js
+++ d3.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const Server = require('./lib/Server');
+const Client = require('./lib/Client');
 const commandLineArgs = require('command-line-args');
 const commandLineUsage = require('command-line-usage');
 const {
@@ -7,72 +9,19 @@ const {
     any: debugAny,
     cli: debug,
   },
+  cli: {
+    options: cliOptions,
+    usage: cliUsage,
+  },
   db,
 } = require('./config');
-const Server = require('./lib/Server');
-const Client = require('./lib/Client');
-const { mix } = require('./lib/mixin');
 
-const optionDefinitions = [
-  {
-    name: 'help',
-    alias: 'h',
-    type: Boolean,
-    description: 'Display this usage guide.'
-  },
-  {
-    name: 'file',
-    alias: 'f',
-    type: String,
-    multiple: false,
-    description: 'Database file to use',
-    typeLabel: '<files>'
-  },
-  {
-    name: 'adduser',
-    alias: 'u',
-    type: String,
-    description: 'Add user'
-  },
-  {
-    name: 'entity',
-    alias: 'e',
-    type: String,
-    description: 'Lookup entity'
-  },
-  {
-    name: 'test',
-    alias: 't',
-    type: Boolean,
-    description: 'Run Test'
-  },
-  {
-    name: 'rpc',
-    alias: 'r',
-    type: Boolean,
-    description: 'List RPC methods'
-  }
-]
-
-const options = commandLineArgs(optionDefinitions)
+const options = commandLineArgs(cliOptions)
 const noOptions = Object.keys(options).length < 1;
 if (!options.db) options.db = db;
 
 if (options.help || noOptions) {
-  const usage = commandLineUsage([
-    {
-      header: 'DA3666',
-      content: 'D3 Demonstration CLI'
-    },
-    {
-      header: 'Options',
-      optionList: optionDefinitions
-    },
-    {
-      content: 'Project home: {underline https://github.com/WAKlNYAN/da3666}\n'+
-        `Database File: ${options.db}`
-    }
-  ])
+  const usage = commandLineUsage(cliUsage)
   console.log(usage)
 } else {
   const server = new Server();
@@ -85,16 +34,14 @@ if (options.help || noOptions) {
 
   server
     .initialize()
-    .then(() => client.initialize())
+    .then(client.initialize.bind(client))
     .then(() => {
       const {
         getEntityById,
         addCommentToEntity,
       } = rpcMethods;
-      if (options.adduser) {
-        console.log('Nope');
-        return Promise.resolve();
-      } else if (options.test) {
+
+      if (options.test) {
         return addCommentToEntity({
           originalEntity: {
             id: 1
@@ -103,9 +50,11 @@ if (options.help || noOptions) {
             text: 'xyzzy'
           }
         }).then(console.log).catch(log);
+
       } else if (options.entity) {
         const id = parseInt(options.entity);
         return getEntityById({ id }).then(console.log);
+
       } else if (options.rpc) {
         console.log(Object.keys(rpcMethods));
         return false;
